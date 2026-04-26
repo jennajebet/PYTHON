4be7a8a7e87589e0bb37e76cce043206a52c2a4e
Jenna <jennamenjo@gmail.com>
2026-04-26 19:28:59 +0300

    Python

---
+++ main.py
@@ -0,0 +1,18 @@
+# printing a greeting message
+# this is a comment 
+print("hello World")
+
+# print my name
+print("my name is Jenna")
+
+# something that i love doing
+print("i love programming!")
+
+# my age
+print("i am 13 years old")
+
+# say how i feel today
+print("i feel happy today")
+
+#print a random message
+print("Error...just kidding")
