94aadd86e3a20cf6d50d7d2087494d7e7b8438ee
Jenna <jennamenjo@gmail.com>
2026-05-02 19:26:06 +0300

    Print

---
+++ activity.py
@@ -0,0 +1,11 @@
+#write a message
+print("Welome to the world of programming")
+
+#print a number
+print(56)
+
+#print a message on your next line
+print("Assata Shakur \n was a member of the black panther movement")
+
+# a number and a word
+print("Crow", 6, "Cow")
