df167603031ae717794c74c71ae45a8ce1f595df
Jenna <jennamenjo@gmail.com>
2026-05-10 19:49:00 +0300

    checking whether greater or smaller

---
+++ activity8.py
@@ -0,0 +1,10 @@
+number = int(input("Enter a number: "))
+
+if(number<15):
+    print(number, "is smaller tan 15")
+    print("This is inside if block")
+else:
+    print(number, "is greater than or equal to 15")
+    print("this is inside else block")
+
+    print("This is outside the if-else block")
