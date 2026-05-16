1b97a17f41614ddc98438115cdbda327775919b6
Jenna <jennamenjo@gmail.com>
2026-05-16 19:39:29 +0300

    Python Operaters

---
+++ activity9.py
@@ -0,0 +1,23 @@
+a=10
+b=12
+c=0
+
+if a and b and c:
+    print("All numbers has a boolean value of True")
+    
+else:
+    print("Atleast one number has a boolean value of False")
+
+    a=10
+    b=-10
+    c=0
+
+    if a > 0 or b >0:
+        print("Either of the numbers is greater than 0")
+    else:
+        print(" no number is greater than 0")
+        
+if b > 0 or c >0:
+    print("Either of the numbers is greater than 0")
+else:
+    print("No number is greater than 0")
