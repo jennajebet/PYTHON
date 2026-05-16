126c6b0c8e54bc5861545fd0549e67cc26060b39
Jenna <jennamenjo@gmail.com>
2026-05-16 20:02:30 +0300

    operatersII

---
+++ activity10.py
@@ -0,0 +1,25 @@
+a=10
+b=12
+c=12
+
+print(not(a==b))
+
+print(not(b==c))
+
+a="python"
+b="coding"
+
+if not(a==b):
+    print(a, "and", b, "are different.")
+
+    a=4
+    b=5
+
+    if not ((a==1)==(b==5)):
+        print("hello")
+
+    a=int(input ("Enter a number: "))
+if  not (a % 2 == 0):
+    print(a, "is not an even number")
+else:
+    print(a, "is an even number")
