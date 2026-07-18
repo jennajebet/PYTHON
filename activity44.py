36ac57348b21e63a0c6193f0ce779fe5b48c49fb
Jenna <jennamenjo@gmail.com>
2026-07-18 19:44:49 +0300

    Advanced python functions

---
+++ activity44.py
@@ -0,0 +1,24 @@
+items = ["pencil", "eraser", "sharpener", "notebook", "glue"]
+stock_counts= [12,0,8,5,3]
+
+inventory={item: count for item, count in zip(items, stock_counts)}
+print("full inventory:", inventory)
+
+in_stock_items=[item for item in items if inventory[item]>0]
+print("items in stock", in_stock_items)
+
+chosen_item= input("which item do you want to buy")
+
+if chosen_item not in inventory or inventory[chosen_item]==0:
+    print(chosen_item, "is out of stock! stopping the checker")
+    exit()
+
+prices=[10,5,40,15,20]
+markup=int(input("enter the markup amoount to add to every price:"))
+
+mark_up_prices= list(map(lambda p : p + markup, prices))
+print("markup prices:", mark_up_prices)
+
+item_index= items.index(chosen_item)
+chosen_price = mark_up_prices[item_index]
+print("Price of:", chosen_item, "after markup:", chosen_price)
