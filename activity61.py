2e965477cf15e20b8138c11370a6161e112c8f38
Jenna <jennamenjo@gmail.com>
2026-08-23 19:38:44 +0300

    BUILDING CONCEPTS

--- activity61.py
+++ activity61.py
@@ -1,33 +1,53 @@
 import pygame
 def main():
     pygame.init()
-    screen=pygame.display.set_mode((500,500))
+    screen_width, screen_height= 500,500
+    screen=pygame.display.set_mode((screen_width,screen_height))
     pygame.display.set_caption("Moving color sprite")
 
     colors = {
-        "white": pygame.color ("white"),
-        "red": pygame.color ("red"),
-        "green": pygame.color ("green"),
-        "blue": pygame.color ("blue"),
-        "yellow": pygame.color ("yellow")
+        "white": pygame.Color ("white"),
+        "red": pygame.Color ("red"),
+        "green": pygame.Color ("green"),
+        "blue": pygame.Color ("blue"),
+        "yellow": pygame.Color ("yellow")
 
     }
 
     current_color=colors["white"]
     x=200
     y=200
-    width=50
-    height=50
+    sprite_width, sprite_height = 60, 60
 
-    clock = pygame.timeclock()
+    clock = pygame.time.Clock()
 
-    running = True
-    while running:
+    done = True
+    while not done:
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
-                running=False
-    Keys = pygame.key.get_pressed()
-    if Keys[pygame.K_LEFT]:
-        x-=5
-        if Keys[pygame.K_RIGHT]:
-        x+=
+                done=False
+        pressed= pygame.key.get_pressed()
+        if pressed[pygame.K_LEFT]: x-=5
+        if pressed[pygame.K_RIGHT]: x+=5
+        if pressed[pygame.K_UP]: y-=5
+        if pressed[pygame.K_DOWN]: y+=5
+
+        x = min(max(0,x), screen_width - sprite_width)
+        y = min(max(0,y), screen_height - sprite_height)
+
+        if x == 0: current_color=colors["white"]
+        elif x == screen_width - sprite_width: current_color = colors["blue"]
+        elif y == 0: current_color=colors["red"]
+        elif y == screen_height - sprite_height: current_color = colors["yellow"]
+        else:
+            current_color=colors["white"]
+        screen.fill(0,0,0)
+        pygame.draw.rect(screen, current_color,(x,y, sprite_width, sprite_height))
+        pygame.display.flip()
+        clock.tick(60)
+    pygame.quit()
+
+if __name__== "__main__":
+    main()
+
+
